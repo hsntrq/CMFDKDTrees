6ab980f678469c29f3802139aacab60e76c46132
Fizza Rubab <fr06161@st.habib.edu.pk>
2021-05-25 13:28:02 +0500

    code converted to modular form

--- main.py
+++ main.py
@@ -1,68 +1,51 @@
 # Parameters: path, n
-from PIL import Image, ImageOps
+from PIL import Image
 from kdTrees import *
 # import concurrent.futures
-import time
-input_image = Image.open("./Image Data Set/36/36.png")
-inputMatrix = input_image.load()
-gray_image = Image.open("./Image Data Set/36/36.png").convert('L')
-outputMap = Image.new(gray_image.mode, gray_image.size)
-outputMatrix = outputMap.load()
-# testImage = Image.new(input_image.mode, input_image.size)
-# testMatrix = testImage.load()
-imageMatrix = gray_image.load()
-width, height = gray_image.size
-featuresList = []
-#                           Constants
-nblock  = 11
-tm = 0.001
-alpha = 9
-t1 = time.perf_counter()
-# Block traversal
-nblock = (nblock//3)*3
-for i in range (0, width-nblock, nblock//3):
-    for j in range(0, height-nblock, nblock//3):
-        # print(i, j)
-        pixelInt =[]
-        # print(i,j, "IJ") 
-        # Sub Blocks Traversal  
-        for m in range(3):
-            for n in range(3):
-                blockSum = 0
-                for k in range(i+m*(nblock//3), i+m*(nblock//3) + (nblock//3)):
-                    for l in range(j+n*(nblock//3), j+n*(nblock//3) + (nblock//3)):
-                        blockSum += imageMatrix[k, l]
-                pixelInt.append(blockSum)
-        pixelInt.append((i,j))
-        featuresList.append(pixelInt)
-    
-#print(featuresList)
-
-kd_tree = KdTree(points = featuresList, k = 9)
-#kd_tree.displayTree()
-t1 = time.perf_counter()
-for block in featuresList:
-    cf = 0
-    nearestBlock = kd_tree.nearestNeigbour(point = block)
-    for i in range(0, nblock):
-        for j in range(0, nblock):
-            rdiff = abs(inputMatrix[block[9][0] + i, block[9][1] + j][0] -  inputMatrix[nearestBlock[9][0] + i, nearestBlock[9][1] + j][0])
-            gdiff = abs(inputMatrix[block[9][0] + i, block[9][1] + j][1] -  inputMatrix[nearestBlock[9][0] + i, nearestBlock[9][1] + j][1])
-            bdiff = abs(inputMatrix[block[9][0] + i, block[9][1] + j][2] -  inputMatrix[nearestBlock[9][0] + i, nearestBlock[9][1] + j][2])
-            if rdiff > alpha or gdiff > alpha or bdiff > alpha :
-                cf += 1
-    if cf <= (nblock**2)*tm:
-        for i in range(0, (1*nblock)):
-            for j in range(0, (1*nblock)):
-                outputMatrix[block[9][0] + i, block[9][1] + j] = 110
-                outputMatrix[nearestBlock[9][0] + i, nearestBlock[9][1] + j] = 255
-        # testImage.show()
-        featuresList.remove(block)
-        # featuresList.remove(nearestBlock)
-# with concurrent.futures.ThreadPoolExecutor() as executor:
-#     executor.map(checkBlock, featuresList)
-t2= time.perf_counter()
-print(t2 - t1)
-outputMap.show()
-outputMap.save("FinalImage.png", "PNG")
+# import time
+def detectForgery(path, name, nblock, tm, alpha):
+    input_image = Image.open(path + "/" + name)
+    inputMatrix = input_image.load()
+    gray_image = Image.open(path + "/" + name).convert('L')
+    outputMap = Image.new(gray_image.mode, gray_image.size)
+    outputMatrix = outputMap.load()
+    imageMatrix = gray_image.load()
+    width, height = gray_image.size
+    featuresList = []
+    # t1 = time.perf_counter()
+    # Block traversal
+    nblock = (nblock//3)*3
+    for i in range (0, width-nblock, nblock//3):
+        for j in range(0, height-nblock, nblock//3):
+            pixelInt =[]
+            for m in range(3):
+                for n in range(3):
+                    blockSum = 0
+                    for k in range(i+m*(nblock//3), i+m*(nblock//3) + (nblock//3)):
+                        for l in range(j+n*(nblock//3), j+n*(nblock//3) + (nblock//3)):
+                            blockSum += imageMatrix[k, l]
+                    pixelInt.append(blockSum)
+            pixelInt.append((i,j))
+            featuresList.append(pixelInt)
+    kd_tree = KdTree(points = featuresList, k = 9)
+    #kd_tree.displayTree()
+    # t1 = time.perf_counter()
+    for block in featuresList:
+        cf = 0
+        nearestBlock = kd_tree.nearestNeigbour(point = block)
+        for i in range(0, nblock):
+            for j in range(0, nblock):
+                rdiff = abs(inputMatrix[block[9][0] + i, block[9][1] + j][0] -  inputMatrix[nearestBlock[9][0] + i, nearestBlock[9][1] + j][0])
+                gdiff = abs(inputMatrix[block[9][0] + i, block[9][1] + j][1] -  inputMatrix[nearestBlock[9][0] + i, nearestBlock[9][1] + j][1])
+                bdiff = abs(inputMatrix[block[9][0] + i, block[9][1] + j][2] -  inputMatrix[nearestBlock[9][0] + i, nearestBlock[9][1] + j][2])
+                if rdiff > alpha or gdiff > alpha or bdiff > alpha :
+                    cf += 1
+        if cf <= (nblock**2)*tm:
+            for i in range(0, (1*nblock)):
+                for j in range(0, (1*nblock)):
+                    outputMatrix[block[9][0] + i, block[9][1] + j] = 110
+                    outputMatrix[nearestBlock[9][0] + i, nearestBlock[9][1] + j] = 255
+            featuresList.remove(block)
+    outputPath = "/media/output/" + name
+    outputMap.save(outputPath, "PNG")
 # outputMap.show()
