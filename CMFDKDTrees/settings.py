3324082bad05899d37a0f4666a245e52b961e82a
hsntrq <mt05491@st.habib.edu.pk>
2021-05-27 21:41:50 +0500

    resolving style errors #6

--- CMFDKDTrees/settings.py
+++ CMFDKDTrees/settings.py
@@ -105,6 +105,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+MIDDLEWARE_CLASSES = [
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
 
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
@@ -116,6 +121,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
